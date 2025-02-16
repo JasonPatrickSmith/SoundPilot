14f27104cd535b89f4e4af36b20daa7574e0b260
JasonPatrickSmith <jasonpatricksmith00@gmail.com>
2025-02-16 18:07:33 -0500

    Started PDF progress

--- Client/scripts/home.js
+++ Client/scripts/home.js
@@ -25,30 +25,27 @@ function toggleClear() {
 // main container
 const main = document.getElementById("main")
 const sections = document.getElementsByClassName("section") // all sections
-let currentsection = 0; // initial button
-let lastpairedsection = document.getElementById("assignments"); // initial section
+let currentsection = -1; // initial button
+let lastpairedsection = null; // initial section
 
 expandbtn.addEventListener("click", (e) => {
     sidebar.classList.toggle("shrunk");
-    main.classList.toggle("shrunk"); //shrinking elements
-
-    main.classList.add("transitioning")
-    setTimeout(endTransition, 300, main)
 
     setTimeout(toggleClear, logo.classList.contains("clear") ? 100 : 0) // making buttons visible or clear
 
-    if (main.classList.contains("shrunk")) {//shrinking or not 
-        lastpairedsection.style.width = "95.6vw"
-    } 
     lastpairedsection.style.opacity = 0;
     setTimeout(() => {
-
+        main.classList.toggle("shrunk"); //shrinking elements
         if (!main.classList.contains("shrunk")) { // main div is shrinking
             lastpairedsection.style.width = "85vw";
+        }
+        else {
+            lastpairedsection.style.width = "95.6vw"
         }
 
         lastpairedsection.style.opacity = 1;
     }, 300)
+
      // hide sections while resizing
 });
 
@@ -63,12 +60,13 @@ for (let i = 0; i < sections.length; i++) {
             pairedsectionid = (sections[i].id).slice(0, -3) // section (like assignments section) that we want to turn visible
             sections[i].classList.add("clicked")
             pairedsection = document.getElementById(pairedsectionid)
-            
             pairedsection.classList.remove("hiddensection")
-
-            lastpairedsection.classList.add("hiddensection")
-            sections[currentsection].classList.remove("clicked")
-
+            
+            if (currentsection != -1) {
+                lastpairedsection.classList.add("hiddensection")
+                sections[currentsection].classList.remove("clicked")
+            }
+            
             currentsection = i
             lastpairedsection = pairedsection
         }
@@ -76,3 +74,9 @@ for (let i = 0; i < sections.length; i++) {
 }
 
 // SIDEBAR
+
+//PDF LOADING
+
+pdfjsLib.getDocument("/Client/pdfs/You'll Be Back - Hamilton TABS.pdf").promise.then(pdf => {
+    console.log("PDF Loaded!")
+})
